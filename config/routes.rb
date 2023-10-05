9e2550d7db6b050c42c2dfb8bf50f6bbb7357031
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-10-05 09:16:05 +0600

    User can sign in:green

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :questions do
     resources :answers, only: [:create, :destroy]
   end
+  root to: "questions#index"
 end
