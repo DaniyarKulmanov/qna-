a441986543e2600afe7a9036de9e2f5afa1fefed
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-10-19 08:06:34 +0600

    User can log out:green

--- spec/features/authentication/sign_in_spec.rb
+++ spec/features/authentication/sign_in_spec.rb
@@ -15,13 +15,13 @@ feature 'User can sign in', %q{
     fill_in 'Password', with: user.password
     click_on 'Log in'
 
-    expect(page).to have_content 'Signed in successfully'
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
   end
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong_user@test.com'
     fill_in 'Password', with: '123456781111'
     click_on 'Log in'
 
-    expect(page).to have_content 'Invalid Email or password.'
+    expect(page).to have_content I18n.t('devise.failure.invalid', authentication_keys: 'Email')
   end
 end
